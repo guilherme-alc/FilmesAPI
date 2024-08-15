21c1b3197235aa9c6e38044395dafeb1ff5e0fb3
guilherme-alc <guilhermealc01@gmail.com>
2024-08-15 15:07:29 -0300

    recuperar por id e retornando dados com boas práticas

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -8,10 +8,27 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> _filmes { get; set; } = new List<Filme>();
+        private static int _id = 1;
         [HttpPost]
-        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        public IActionResult  CreateFilme ([FromBody] Filme filme)
         {
+            filme.Id = _id++;
             _filmes.Add(filme);
+            return CreatedAtAction(nameof(GetByIdFilme), new { filmeId = filme.Id }, filme);
+        }
+        [HttpGet]
+        public IActionResult GetFilmes()
+        {
+            return Ok(_filmes);
+        }
+        [HttpGet("{filmeId}")]
+        public IActionResult GetByIdFilme(int filmeId) 
+        {
+            Filme filme = _filmes.FirstOrDefault(filme => filme.Id == filmeId);
+
+            if (filme is null)
+                return NotFound("Filme não encontrado.");
+
             return Ok(filme);
         }
     }
