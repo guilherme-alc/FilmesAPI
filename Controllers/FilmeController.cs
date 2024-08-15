9a526f1112df1b83115ae917a8bbae40e424f283
guilherme-alc <guilhermealc01@gmail.com>
2024-08-15 14:32:06 -0300

    criação do projeto com restrições dos parâmetros em filme

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> _filmes { get; set; } = new List<Filme>();
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            _filmes.Add(filme);
+            return Ok(filme);
+        }
+    }
+}
