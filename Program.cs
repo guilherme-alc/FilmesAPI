0f429cc5f03ef5c9a92c86e81009320a436e6be4
guilherme-alc <guilhermealc01@gmail.com>
2024-08-15 15:59:19 -0300

    configurando contexto do ef para conexão do banco de dados e mapeamento da tabela Filmes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilmesAPI
 {
@@ -13,6 +15,9 @@ namespace FilmesAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var connectString = builder.Configuration["ConnectionStrings:UsuarioConnection"];
+            builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(connectString));
 
             var app = builder.Build();
 
